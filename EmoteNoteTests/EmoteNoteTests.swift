837985c75b6cb843f6efd755ff6da8b379bc8d96
William Leahy <Will@MacBook-Pro-2.local>
2017-04-17 13:30:00 -0400

    Initial Commit

---
+++ EmoteNoteTests/EmoteNoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  EmoteNoteTests.swift
+//  EmoteNoteTests
+//
+//  Created by William Leahy on 4/17/17.
+//  Copyright © 2017 William Leahy. All rights reserved.
+//
+
+import XCTest
+@testable import EmoteNote
+
+class EmoteNoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
