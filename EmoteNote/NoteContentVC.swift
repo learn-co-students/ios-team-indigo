3fe90669d10e4c3f9489240b821a822d65f7319f
William Leahy <Will@MacBook-Pro-2.local>
2017-04-24 12:03:08 -0400

    add scale, color + bugfix

--- EmoteNote/NoteContentVC.swift
+++ EmoteNote/NoteContentVC.swift
@@ -91,11 +91,32 @@ class NoteContentVC: UIViewController, IAxisValueFormatter {
         let chartDataSet = BarChartDataSet(values: dataEntries, label: "Emotion Index")
         let chartData = BarChartData(dataSet: chartDataSet)
         
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        chartDataSet.colors = colors
         
         barChartView.data = chartData
         barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:emotionIndex)
         barChartView.xAxis.granularity = 1
         
+        barChartView.chartDescription?.text = ""
+        barChartView.leftAxis.axisMaximum = 1.0
+        barChartView.rightAxis.axisMaximum = 1.0
+    
+        
+        // MARK : animation
+        let animationDouble = 1.5
+        barChartView.animate(xAxisDuration: animationDouble, yAxisDuration: animationDouble)
+    
     }
     
     func stringForValue(_ value: Double, axis: AxisBase?) -> String {
